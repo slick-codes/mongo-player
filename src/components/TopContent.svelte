120f487b867ead413ff03ef4497987af2280157b
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-07 09:03:49 +0100

    add TopContent.svelte component

---
+++ src/components/TopContent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import RightPanel from './RightPanel.svelte';
+	import SideBar from './../components/SideBar.svelte';
+</script>
+
+
+<div class="top-content">
+    <SideBar />
+    <RightPanel />
+</div>
+
+<style lang="scss">
+    .top-content{
+        height:100%;
+        display:flex;
+    }
+</style>

510f8cc7350a8bdda1b4cf4921c85b91219006dc
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-07 09:01:03 +0100

    add RightPanel.svelte components

---
+++ src/components/RightPanel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+
+<div class="right-panel">
+
+</div>
+
+<style lang="scss">
+    .right-panel{
+        padding-top:3em;
+        width:100%;
+        padding-right:3em;
+        background:purple;
+    }
+
+</style>

c13b66b935b1c5dc64bdc0696c0613052a800972
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-07 09:06:26 +0100

    add Home.svelte component

---
+++ src/components/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+	import TopContent from './../components/TopContent.svelte';
+	import Controller from './../components/Controller.svelte';
+
+
+</script>
+
+<div class="parents-container">
+    <TopContent />
+    <Controller />
+</div>
+
+<style lang="scss">
+    .parents-container{
+        width: 100%;
+        display:flex;
+        flex-flow:column;
+        height:100%;
+    }
+</style>

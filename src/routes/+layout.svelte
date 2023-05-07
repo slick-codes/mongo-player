3e58c3a9a06e05849c56a9dae020c271954ed5b3
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-07 09:06:57 +0100

    add initial route files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,25 +3,21 @@
 	import '../app.postcss';
 	import '../app.postcss';
 	let darkmode: boolean;
-
-	isDarkMode.subscribe((value: boolean) => (darkmode = value));
-	let about = ` Hello, my name is Paul Ezekiel-Hart and I am an experienced MEVN stack developer, Brand Identity, and Product Designer with ${
-		new Date().getFullYear() - 2018 + 1
-	} years of working experience, as a freelancer and a Software Engineer at Technoville. I am open to freelancing, remote jobs, and collaboration.`;
 </script>
 
 <svelte:head>
 	<title>Paul Ezekiel-Hart Portfolio Website</title>
-	<meta name="theme-color" content={darkmode ? '#191627' : '#eeeeee'} />
+	<!-- <meta name="theme-color" content={darkmode ? '#191627' : '#eeeeee'} />
 	<meta name="description" content={about} />
 
 	<meta property="og:title" content="Paul Ezekiel-Hart Portfolio" />
 	<meta property="og:description" content={about} />
-	<meta property="og:image" content="/images/avatar.png" />
+	<meta property="og:image" content="/images/avatar.png" /> -->
 </svelte:head>
 <main>
 	<slot />
 </main>
 
 <style global lang="scss">
+	@import url("../assets/scss/main.scss")
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,16 @@
 	import Home from './../components/Home.svelte';
 </script>
 
-<main class="" >
+<main  >
 	<Home />
 </main>
 
 <style lang="scss">
+	main{
+        // padding:1em;
+        background:#0f141e;
+		height:100vh;
+		width: 100%;
+		// padding:2em;
+	}
 </style>

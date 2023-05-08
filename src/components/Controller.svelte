7489903aa7d6c069eb8c49b0778724e9530f6def
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-08 22:38:36 +0100

    Add curresponding code

--- src/components/Controller.svelte
+++ src/components/Controller.svelte
@@ -1,70 +1,67 @@
 <script lang="ts">
-
 </script>
 
-
 <div class="controller">
-    <div class="controller__content">
-        <div class="controller__button_container"></div>
-        <div class="controller__progresssbar-container">
-            <div class="controller__bar">
-                <div class="progressbar">
-                    <div></div>
-                </div>
-            </div>
-            <div class="timestamp__container">
-                <div>2:15</div>
-                <div>2:15</div>
-            </div>
-        </div>
-    </div>
+	<div class="controller__content">
+		<div class="controller__button_container" />
+		<div class="controller__progresssbar-container">
+			<div class="controller__bar">
+				<div class="progressbar">
+					<div />
+				</div>
+			</div>
+			<div class="timestamp__container">
+				<div>2:15</div>
+				<div>2:15</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
-    @import "./../assets/scss/config";
-    .controller{
-        height:10em;
-        // background:blue;
-        padding: $side-padding;
-        padding-top:0;
+	@import './../assets/scss/config';
+	.controller {
+		height: 10em;
+		// background:blue;
+		padding: $side-padding;
+		padding-top: 0;
 
-        &__content{
-            // background:red;
-            height:100%;
-        }
+		&__content {
+			// background:red;
+			height: 100%;
+		}
 
-        .controller__button_container{
-            padding:2em;
-        }
+		.controller__button_container {
+			padding: 2em;
+		}
 
-        .controller__bar{
+		.controller__bar {
+			.progressbar {
+				width: 100%;
+				height: 0.2em;
+				background: rgba(255, 255, 255, 0.175);
+				overflow: hidden;
+				border-radius: 4em;
+				transition: transform 0.3s;
 
-            .progressbar{
-                width:100%;
-                height:.2em;
-                background:rgba(255, 255, 255, 0.097);
-                overflow:hidden;
-                border-radius:4em;
-                transition: transform .3s;
+				&:hover {
+					transform: scaleY(1.8);
+				}
 
-                &:hover{
-                    transform:scaleY(1.8);
-                }
+				> div {
+					background: rgb(219, 219, 219);
+					height: 3em;
+					width: 3em;
+				}
+			}
+		}
 
-                > div{
-                    background:rgb(219, 219, 219);
-                    height:3em;
-                    width:3em;
-                }
-            }
-        }
-
-        .timestamp__container{
-            display:flex;
-            justify-content: space-between;
-            color:white;
-            padding-top:.5em;
-            font-size:.9rem;
-        }
-    }
+		.timestamp__container {
+			display: flex;
+			justify-content: space-between;
+			color: white;
+			padding-top: 0.5em;
+			font-size: 0.9rem;
+		}
+	}
 </style>

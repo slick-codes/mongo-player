8e1259a0f91d3c7f495a9c1055cfb48cb0b25d53
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-08 22:12:18 +0100

    add getMetaData and getAudio method

---
+++ electron/src/preload.js
@@ -0,0 +1,55 @@
+const { contextBridge } = require('electron')
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const NodeID3 = require('node-id3')
+
+
+const directories = [
+    path.join(os.homedir(), "Music"),
+    // path.join(os.homedir(), "Documents"),
+    path.join(os.homedir(), "Downloads"),
+    path.join(os.homedir(), "Desktop")
+]
+
+
+const acceptedFileType = [".mp3", ".ogg", ".wav"]
+
+let mp3Files = [];
+
+const getAudio = async function (directories) {
+    //
+
+    directories.forEach(directory => {
+        fs.readdirSync(directory).forEach(async file => {
+            const filePath = path.join(directory, file)
+            const fileExt = path.extname(file)
+            let fileStat = fs.statSync(filePath)
+
+            if (fileStat.isDirectory()) {
+                const result = await getAudio([filePath])
+                mp3Files = [...mp3Files, ...result]
+            }
+
+            if (fileStat.isFile() && acceptedFileType.includes(fileExt))
+                mp3Files.push({
+                    file: filePath,
+                    ...NodeID3.read(filePath)
+                })
+        })
+    })
+
+    return mp3Files
+}
+
+
+
+
+contextBridge.exposeInMainWorld("api", {
+    getAudio: () => getAudio(directories),
+    appName: "mongo player",
+    getMetaData: (metaData) => (metaData.map(value => ({ path: value, ...NodeID3.read(value) })))
+})
+
+
+

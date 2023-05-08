58ffd72250c3dabb60c8c034241df3177a575979
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-08 22:39:42 +0100

    add Audiolist.svelte file

---
+++ src/components/AudioList.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+    import audios from './../store/audio.json';
+    // console.log(audios)
+
+    
+    async function getAudioData(){
+        if( browser ){
+            const audio = await window.api.getAudio()
+            return audio
+        }
+    }
+
+    // Promise.resolve(getAudioData()).then(console.log)
+    
+</script>
+
+<div class="container__audiolist">
+</div>
+
+<style lang="scss">
+
+</style>

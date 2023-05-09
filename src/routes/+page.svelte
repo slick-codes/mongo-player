9e677ddc5930137fe5be4d22e23136f0b21a9923
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-09 13:12:50 +0100

    import Home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,15 @@
 	import Home from './../components/Home.svelte';
 </script>
 
-<main  >
+<main>
 	<Home />
 </main>
 
 <style lang="scss">
-	main{
-        // padding:1em;
-        background:#0f141e;
-		height:100vh;
+	main {
+		// padding:1em;
+		background: #0f141e;
+		height: 100vh;
 		width: 100%;
 		// padding:2em;
 	}

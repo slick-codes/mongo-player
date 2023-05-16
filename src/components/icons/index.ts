ab6a903503ef1ed599549fc1cf3f337167a31b89
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-16 16:51:05 +0100

    import all icon to index

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,6 +2,10 @@ import Pause from './Pause.svelte'
 import Play from './Play.svelte'
 import Previous from './Previous.svelte'
 import Next from './Next.svelte'
+import ArrowRepeat from './ArrowRepeat.svelte'
+import ArrowRepeatOnce from './ArrowRepeatOnce.svelte'
+import RandomIcon from './RandomIcon.svelte'
+import VolumeUp from './VolumeUp.svelte'
+import VolumeOff from './VolumeOff.svelte'
 
-
-export { Pause, Play, Previous, Next }
+export { Pause, Play, Previous, Next, ArrowRepeat, ArrowRepeatOnce, RandomIcon, VolumeUp, VolumeOff }

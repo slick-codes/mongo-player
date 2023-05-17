03c0455ddc48ee82db21331fd7b0122bcf972d55
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-17 07:26:08 +0100

    import the loadingScreen component

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,11 +1,13 @@
 <script>
 	import TopContent from './../components/TopContent.svelte';
 	import Controller from './../components/Controller.svelte';
+	import LoadingScreen from './../components/LoadingScreen.svelte'
 </script>
 
 <div class="parents-container">
 	<TopContent />
 	<Controller />
+	<LoadingScreen />
 </div>
 
 <style lang="scss">

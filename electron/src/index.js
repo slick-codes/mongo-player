5f839348d49c684fd29908aacaeabb87ed9cee3f
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2023-05-31 15:21:11 +0100

    fix codes

--- electron/src/index.js
+++ electron/src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
-const methods = require('./methods')
+const methods = require('./methods.js')
 const url = require('url')
 
 
@@ -34,27 +34,26 @@ app.whenReady().then(function () {
 
 
     // Zoom out by 25%
-    // window.webContents.setZoomFactor(0.90);
+    // window.webContents.setZoomFactor(0.2);
 
     // this is only for development purpose
     window.on("ready-to-show", window.show)
 
     if (process.env.DEVELOPMENT) {
         window.setPosition(1618, 180)
-        // window.setFullScreen(true);
         window.loadURL("http://localhost:5173")
+        // window.setFullScreen(true);
         window.webContents.openDevTools()
     } else {
-        // window.loadFile(path.join(__dirname, "../../build/index.html"))
+        window.loadFile(path.join(__dirname, "build", "index.html"))
         let appdir = app.getAppPath()
-
-        window.loadURL(
-            url.format({
-                pathname: path.join(appdir, "build", "index.html"),
-                protocol: "file:",
-                slashes: true
-            })
-        )
+        // window.loadURL(
+        //     url.format({
+        //         pathname: path.join(appdir, "build", "index.html"),
+        //         protocol: "file:",
+        //         slashes: true
+        //     })
+        // )
     }
 
 
